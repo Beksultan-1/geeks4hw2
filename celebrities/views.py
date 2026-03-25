b202c581566fe05ae5d26c95a543942aa99d8cad
unknown <bektursejitbekov@gmail.com>
2026-03-25 23:58:22 +0600

    HW2: final view fix

--- celebrities/views.py
+++ celebrities/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Celebrity
 
 def celebrity_list(request):
     celebrities = Celebrity.objects.all()
     return render(request, 'celebrity_list.html', {'celebrities': celebrities})
+
+def celebrity_detail(request, id):
+    star = get_object_or_404(Celebrity, id=id)
+    return render(request, 'celebrity_detail.html', {'star': star})
